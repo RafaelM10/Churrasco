3b83d7939bf1270ef4168ce07a8237985368cfd4
meiradev <rafameira10@icloud.com>
2022-07-26 16:52:16 -0300

    Configurando input

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -20,14 +20,54 @@ export const ModalContainer = styled.div<PropsModal>`
 export const ModalContent = styled.div`
   background-color: #fefefe;
   margin: 15% auto;
-  padding: 20px;
   border: 1px solid #888;
-  width: 50%;
+  width: 100%;
+  border-radius: 4px;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    padding: 2px 1.5rem;
+    height: 4rem;
+    margin: 30px 11px;
+
+    border-radius: 0.25rem;
+    border: 1px solid #d7d7d7;
+
+    background: #e7e9ee;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: #888;
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button {
+    max-width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: #e7e9ee;
+    margin: 9px 11px;
+    border-radius: 0.25rem;
+    border: 1px solid #d7d7d7;
+    font-size: 1rem;
+    font-weight: 600;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
 
 export const ModalClose = styled.span`
   color: #aaa;
-  float: right;
+  text-align: right;
   font-size: 28px;
   font-weight: bold;
 
