7d796006ac1d22dc1d768a5577ca792c7fbf265f
meiradev <rafameira10@icloud.com>
2022-08-23 13:43:52 -0300

    feat: add estilizando card

--- src/pages/Details/styles.tsx
+++ src/pages/Details/styles.tsx
@@ -1,11 +1,25 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+`;
 
-`
-export const Border = styled.div`
+interface CardProps {
+  schedule?: boolean;
+}
 
-`
-export const Input = styled.div`
+export const Border = styled.div<CardProps>`
+  padding: 25px;
+  height: 200px;
+  grid-column-end: span 6;
+  background: #fff;
+  box-shadow: 5px 0px 16px rgb(0 0 0 / 29%);
+  border-radius: 2px;
+  position: absolute;
+  top: -29px;
+`;
 
-`
+export const Input = styled.div``;
